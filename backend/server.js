640dcd075fcffec537c220156c88837a64d4e4d5
Punyo Sunya <sunya22839@example.com>
2024-12-29 21:39:53 +0530

    edited server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const connectDB = require('./config/db');
 const stockRoutes = require('./routes/stockRoutes');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // connecting to the database
 connectDB();
